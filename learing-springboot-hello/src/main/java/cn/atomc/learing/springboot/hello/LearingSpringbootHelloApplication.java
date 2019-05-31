ab92b65e27aea492decce0c87460802710a5d70e
pangyannan <pangyannan@126.com>
2019-06-01 01:02:28 +0800

    新增helloworld

---
+++ learing-springboot-hello/src/main/java/cn/atomc/learing/springboot/hello/LearingSpringbootHelloApplication.java
@@ -0,0 +1,13 @@
+package cn.atomc.learing.springboot.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearingSpringbootHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearingSpringbootHelloApplication.class, args);
+    }
+
+}
