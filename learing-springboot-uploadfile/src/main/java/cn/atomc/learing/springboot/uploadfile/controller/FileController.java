97d5b6d79ce0924c2baf5a1cde84a97146a0fecf
pangyannan <pangyannan@126.com>
2019-06-30 23:07:18 +0800

    附件上传

---
+++ learing-springboot-uploadfile/src/main/java/cn/atomc/learing/springboot/uploadfile/controller/FileController.java
@@ -0,0 +1,103 @@
+package cn.atomc.learing.springboot.uploadfile.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * @author Eric
+ * @create 2019-06-30
+ */
+@Controller
+public class FileController {
+    //定义附件上传的根目录
+    private final  static  String UPLOADED_FOLDER = "E:\\uploadFile\\";
+
+    /**
+     * 上传页面
+     * @return
+     */
+    @GetMapping("/")
+    public String index() {
+        return "upload";
+    }
+
+    /**
+     * 上传状态回显
+     * @return
+     */
+    @GetMapping("uploadStatus")
+    public String uploadStatus() {
+        return "uploadStatus";
+    }
+
+    /**
+     * 上传业务处理
+     * @param file
+     * @param redirectAttributes
+     * @return
+     */
+    @PostMapping("/upload")
+    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+        try {
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            // UPLOADED_FOLDER ⽂文件本地存储地址
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+            redirectAttributes.addFlashAttribute("message","You successfully uploaded '" + file.getOriginalFilename() + "'");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "redirect:/uploadStatus";
+    }
+
+
+    /**
+     * 批量上传页面
+     * @return
+     */
+    @GetMapping("/more")
+    public String more() {
+        return "uploadMore";
+    }
+
+    /**
+     * 批量上传逻辑
+     * @param files
+     * @param redirectAttributes
+     * @return
+     */
+    @PostMapping("/uploadMore")
+    public String moreFileUpload(@RequestParam("file") MultipartFile[] files,
+                                 RedirectAttributes redirectAttributes) {
+        if (files.length==0) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+        for(MultipartFile file:files){
+            try {
+                byte[] bytes = file.getBytes();
+                Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+                Files.write(path, bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        redirectAttributes.addFlashAttribute("message", "You successfully uploaded all");
+        return "redirect:/uploadStatus";
+    }
+}
